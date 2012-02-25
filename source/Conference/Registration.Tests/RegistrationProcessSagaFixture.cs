1c032b242f5a283a2dc4a6807ce57c637ead8f2e
Julian Dominguez <julian_domin@hotmail.com>
2012-02-24 16:12:25 -0800

    RegistrationProcessSaga to make a reservation (failing test, not implemented yet)

---
+++ source/Conference/Registration.Tests/RegistrationProcessSagaFixture.cs
@@ -0,0 +1,46 @@
+﻿// ==============================================================================================================
+// Microsoft patterns & practices
+// CQRS Journey project
+// ==============================================================================================================
+// Copyright (c) Microsoft Corporation and contributors http://cqrsjourney.github.com/contributors/members
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+// ==============================================================================================================
+
+namespace Registration.Tests
+{
+    using System;
+
+    using Registration.Commands;
+    using Xunit;
+
+    public class given_uninitialized_saga
+    {
+        private RegistrationProcessSaga sut;
+        private FakeBus bus;
+
+        public given_uninitialized_saga()
+        {
+            this.bus = new FakeBus();
+            this.sut = new RegistrationProcessSaga(bus);
+        }
+
+        [Fact]
+        public void when_starting_saga_then_locks_seats()
+        {
+            var registerCommand = new RegisterToConference
+                {
+                    ConferenceId = Guid.NewGuid(),
+                    NumberOfSeats = 1
+                };
+
+            sut.Handle(registerCommand);
+
+            Assert.Equal(1, bus.SentCommands.Count);
+            Assert.IsAssignableFrom<MakeReservation>(bus.SentCommands[0]);
+        }
+    }
+}
