5ff53b2df478cd82558ee23371bef4d8f064346c
Daniel Cazzulino <daniel@cazzulino.com>
2012-03-06 10:28:18 -0300

    Renamed MessageHandlers->Handlers (folder)

---
+++ source/Conference/Registration/Handlers/RegistrationProcessSagaHandler.cs
@@ -0,0 +1,61 @@
+﻿// ==============================================================================================================
+// Microsoft patterns & practices
+// CQRS Journey project
+// ==============================================================================================================
+// Copyright (c) Microsoft Corporation and contributors http://cqrsjourney.github.com/contributors/members
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+// ==============================================================================================================
+
+namespace Registration.MessageHandlers
+{
+	using Common;
+	using Registration.Commands;
+	using Registration.Events;
+
+	// TODO: this is to showcase how a handler could be written. No unit tests created yet. Do ASAP.
+	public class RegistrationProcessSagaHandler : IEventHandler<OrderPlaced>, IEventHandler<ReservationAccepted>, IEventHandler<ReservationRejected>, ICommandHandler<ExpireReservation>
+	{
+		private ISagaRepository repository;
+
+		public RegistrationProcessSagaHandler(ISagaRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		public void Handle(OrderPlaced @event)
+		{
+			var saga = new RegistrationProcessSaga();
+			saga.Handle(@event);
+
+			this.repository.Save(saga);
+		}
+
+		public void Handle(ReservationAccepted @event)
+		{
+			var saga = this.repository.Find<RegistrationProcessSaga>(@event.ReservationId);
+			saga.Handle(@event);
+
+			this.repository.Save(saga);
+		}
+
+		public void Handle(ReservationRejected @event)
+		{
+			var saga = this.repository.Find<RegistrationProcessSaga>(@event.ReservationId);
+			saga.Handle(@event);
+
+			this.repository.Save(saga);
+		}
+
+		public void Handle(ExpireReservation command)
+		{
+			var saga = this.repository.Find<RegistrationProcessSaga>(command.Id);
+			saga.Handle(command);
+
+			this.repository.Save(saga);
+		}
+	}
+}
