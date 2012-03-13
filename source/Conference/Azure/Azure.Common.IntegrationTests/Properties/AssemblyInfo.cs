3fb2fb63e9c02a3186e3528a43eb10d0103749fe
Daniel Cazzulino <daniel@cazzulino.com>
2012-03-13 15:17:40 -0300

    Fix line endings

--- source/Conference/Azure/Azure.Common.IntegrationTests/Properties/AssemblyInfo.cs
+++ source/Conference/Azure/Azure.Common.IntegrationTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Azure.Common.IntegrationTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright � Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
