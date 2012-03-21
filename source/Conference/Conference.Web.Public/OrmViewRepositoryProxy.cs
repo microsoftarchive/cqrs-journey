7b912136b470b75f418eadae605669c3489f53f9
Mark Seemann <mark@seemann.ms>
2012-03-21 21:18:36 +0100

    Added a Virtual Proxy for OrmViewRepository.
    The OrmViewRepositoryProxy is a pure infrastructure component, so I
    decided to place it in the Composition Root of the application.
    
    It's a Humble Object, so I didn't add any unit tests.
    
    All unit tests are still green.

---
+++ source/Conference/Conference.Web.Public/OrmViewRepositoryProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Common;
+using Registration.ReadModel;
+
+namespace Conference.Web.Public
+{
+    public class OrmViewRepositoryProxy : IViewRepository
+    {
+        public T Find<T>(Guid id) where T : class
+        {
+            using (var repo = new OrmViewRepository())
+                return repo.Find<T>(id);
+        }
+
+        public IQueryable<T> Query<T>() where T : class
+        {
+            using (var repo = new OrmViewRepository())
+                return repo.Query<T>();
+        }
+    }
+}
