f1fe0cadf2fc138a0f007e3af67f13f0d9702348
Daniel Cazzulino (kzu) <daniel@cazzulino.com>
2012-04-11 13:42:20 -0300

    Added solution configuration so that web project builds in DebugLocal to run with the in-memory buses.

--- source/Conference/Common/MemoryEventBus.cs
+++ source/Conference/Common/MemoryEventBus.cs
@@ -13,51 +13,49 @@
 
 namespace Common
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-	public class MemoryEventBus : IEventBus
-	{
-		private List<IEventHandler> handlers = new List<IEventHandler>();
-		private List<IEvent> events = new List<IEvent>();
+    public class MemoryEventBus : IEventBus
+    {
+        private List<IEventHandler> handlers = new List<IEventHandler>();
+        private List<IEvent> events = new List<IEvent>();
 
-		public MemoryEventBus(params IEventHandler[] handlers)
-		{
-			this.handlers.AddRange(handlers);
-		}
+        public MemoryEventBus(params IEventHandler[] handlers)
+        {
+            this.handlers.AddRange(handlers);
+        }
 
-		public void Register(IEventHandler handler)
-		{
-			this.handlers.Add(handler);
-		}
+        public void Register(IEventHandler handler)
+        {
+            this.handlers.Add(handler);
+        }
 
-		public void Publish(IEvent @event)
-		{
-			this.events.Add(@event);
+        public void Publish(IEvent @event)
+        {
+            this.events.Add(@event);
 
-			Task.Factory.StartNew(() =>
-			{
-				var handlerType = typeof(IEventHandler<>).MakeGenericType(@event.GetType());
+            Task.Factory.StartNew(() =>
+            {
+                var handlerType = typeof(IEventHandler<>).MakeGenericType(@event.GetType());
 
-				foreach (dynamic handler in this.handlers
-					.Where(x => handlerType.IsAssignableFrom(x.GetType())))
-				{
-					handler.Handle((dynamic)@event);
-				}
-			});
-		}
+                foreach (dynamic handler in this.handlers
+                    .Where(x => handlerType.IsAssignableFrom(x.GetType())))
+                {
+                    handler.Handle((dynamic)@event);
+                }
+            });
+        }
 
-		public void Publish(IEnumerable<IEvent> events)
-		{
-			foreach (var @event in events)
-			{
-				this.Publish(@event);
-			}
-		}
+        public void Publish(IEnumerable<IEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                this.Publish(@event);
+            }
+        }
 
-		public IEnumerable<IEvent> Events { get { return this.events; } }
-	}
+        public IEnumerable<IEvent> Events { get { return this.events; } }
+    }
 }
