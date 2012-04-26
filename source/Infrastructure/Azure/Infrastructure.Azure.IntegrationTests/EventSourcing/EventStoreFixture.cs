bcb09cbe1462f7114a707fa61bb2bc12442d4a0b
Julian Dominguez <julian_domin@hotmail.com>
2012-04-26 15:29:06 -0700

    Basic Azure tables event store

---
+++ source/Infrastructure/Azure/Infrastructure.Azure.IntegrationTests/EventSourcing/EventStoreFixture.cs
@@ -0,0 +1,152 @@
+﻿// ==============================================================================================================
+// Microsoft patterns & practices
+// CQRS Journey project
+// ==============================================================================================================
+// ©2012 Microsoft. All rights reserved. Certain content used with permission from contributors
+// http://cqrsjourney.github.com/contributors/members
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+// ==============================================================================================================
+
+namespace Azure.IntegrationTests.EventSourcing
+{
+    using System;
+    using System.Linq;
+    using Infrastructure.Azure;
+    using Infrastructure.Azure.EventSourcing;
+    using Infrastructure.Azure.Messaging;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.StorageClient;
+    using Xunit;
+
+    public class EventStoreFixture : IDisposable
+    {
+        private readonly string tableName;
+        private CloudStorageAccount account;
+        private EventStore sut;
+
+        public EventStoreFixture()
+        {
+            this.tableName = "EventStoreFixture" + new Random((int)DateTime.Now.Ticks).Next();
+            var settings = InfrastructureSettings.ReadEventSourcing("Settings.xml");
+            this.account = CloudStorageAccount.Parse(settings.ConnectionString);
+            this.sut = new EventStore(this.account, this.tableName);
+        }
+
+        [Fact]
+        public void can_save_and_load_one_event()
+        {
+            var id = Guid.NewGuid().ToString();
+            var e = new EventData
+                {
+                    Version = 1,
+                    EventType = "Test",
+                    Payload = "Payload",
+                };
+
+            sut.Save(id, new[] { e });
+
+            var stored = sut.Load(id, 0).ToList();
+
+            Assert.Equal(1, stored.Count);
+            Assert.Equal(e.Version, stored[0].Version);
+            Assert.Equal(e.EventType, stored[0].EventType);
+            Assert.Equal(e.Payload, stored[0].Payload);
+        }
+
+        [Fact]
+        public void can_load_multiple_events_in_order()
+        {
+            var id = Guid.NewGuid().ToString();
+            var events = new[]
+                             {
+                                 new EventData { Version = 1, EventType = "Test1", Payload = "Payload1" },
+                                 new EventData { Version = 2, EventType = "Test2", Payload = "Payload2" },
+                                 new EventData { Version = 3, EventType = "Test3", Payload = "Payload3" },
+                             };
+
+            sut.Save(id, events);
+
+            var stored = sut.Load(id, 0).ToList();
+
+            Assert.Equal(3, stored.Count);
+            Assert.Equal(1, stored[0].Version);
+            Assert.Equal(2, stored[1].Version);
+            Assert.Equal(3, stored[2].Version);
+            Assert.Equal("Payload1", stored[0].Payload);
+            Assert.Equal("Payload2", stored[1].Payload);
+            Assert.Equal("Payload3", stored[2].Payload);
+        }
+
+        [Fact]
+        public void can_load_events_stored_at_different_times()
+        {
+            var id = Guid.NewGuid().ToString();
+            var e1 = new EventData { Version = 1, EventType = "Test1", Payload = "Payload1" };
+            var e2 = new EventData { Version = 2, EventType = "Test2", Payload = "Payload2" };
+            var e3 = new EventData { Version = 3, EventType = "Test3", Payload = "Payload3" };
+
+            sut.Save(id, new[] { e1, e2 });
+            sut.Save(id, new[] { e3 });
+
+            var stored = sut.Load(id, 0).ToList();
+
+            Assert.Equal(3, stored.Count);
+            Assert.Equal(1, stored[0].Version);
+            Assert.Equal(2, stored[1].Version);
+            Assert.Equal(3, stored[2].Version);
+            Assert.Equal("Payload1", stored[0].Payload);
+            Assert.Equal("Payload2", stored[1].Payload);
+            Assert.Equal("Payload3", stored[2].Payload);
+        }
+
+        [Fact]
+        public void can_load_events_since_specified_version()
+        {
+            var id = Guid.NewGuid().ToString();
+            var events = new[]
+                             {
+                                 new EventData { Version = 1, EventType = "Test1", Payload = "Payload1" },
+                                 new EventData { Version = 2, EventType = "Test2", Payload = "Payload2" },
+                                 new EventData { Version = 3, EventType = "Test3", Payload = "Payload3" },
+                             };
+
+            sut.Save(id, events);
+
+            var stored = sut.Load(id, 2).ToList();
+
+            Assert.Equal(2, stored.Count);
+            Assert.Equal(2, stored[0].Version);
+            Assert.Equal(3, stored[1].Version);
+            Assert.Equal("Payload2", stored[0].Payload);
+            Assert.Equal("Payload3", stored[1].Payload);
+        }
+
+        [Fact]
+        public void cannot_store_same_version()
+        {
+            var id = Guid.NewGuid().ToString();
+            var e1 = new EventData { Version = 1, EventType = "Test1", Payload = "Payload1" };
+            var e2 = new EventData { Version = 1, EventType = "Test2", Payload = "Payload2" };
+
+            sut.Save(id, new[] { e1 });
+
+            Assert.Throws<ConcurrencyException>(() => sut.Save(id, new[] { e2 }));
+
+            var stored = sut.Load(id, 0).ToList();
+
+            Assert.Equal(1, stored.Count);
+            Assert.Equal(1, stored[0].Version);
+            Assert.Equal("Payload1", stored[0].Payload);
+        }
+
+        public void Dispose()
+        {
+            var client = this.account.CreateCloudTableClient();
+            client.DeleteTableIfExist(this.tableName);
+        }
+    }
+}
---
+++ source/Infrastructure/Azure/Infrastructure.Azure/EventSourcing/EventStore.cs
@@ -0,0 +1,107 @@
+﻿// ==============================================================================================================
+// Microsoft patterns & practices
+// CQRS Journey project
+// ==============================================================================================================
+// ©2012 Microsoft. All rights reserved. Certain content used with permission from contributors
+// http://cqrsjourney.github.com/contributors/members
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+// with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is 
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+// ==============================================================================================================
+
+namespace Infrastructure.Azure.EventSourcing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data.Services.Client;
+    using System.Linq;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.StorageClient;
+
+    public class EventStore
+    {
+        private readonly CloudStorageAccount account;
+        private readonly string tableName;
+        private readonly CloudTableClient tableClient;
+
+        public EventStore(CloudStorageAccount account, string tableName)
+        {
+            this.account = account;
+            this.tableName = tableName;
+            this.tableClient = account.CreateCloudTableClient();
+            
+            // TODO: error handling
+            tableClient.CreateTableIfNotExist(tableName);
+        }
+
+        public IEnumerable<EventData> Load(string partitionKey, int version)
+        {
+            var context = this.tableClient.GetDataServiceContext();
+            var formattedVersion = version.ToString("D10");
+            var query = context.CreateQuery<EventEntity>(this.tableName).Where(x => x.PartitionKey == partitionKey && x.RowKey.CompareTo(formattedVersion) >= 0);
+
+            // TODO: error handling, continuation tokens, etc
+            var all = query.AsTableServiceQuery().Execute();
+            return all.Select(x => new EventData
+                                       {
+                                           // TODO: skip if version is not a number
+                                           Version = int.Parse(x.RowKey),
+                                           EventType = x.EventType,
+                                           Payload = x.Payload
+                                       });
+        }
+
+        public void Save(string partitionKey, IEnumerable<EventData> events)
+        {
+            var context = this.tableClient.GetDataServiceContext();
+            foreach (var eventData in events)
+            {
+                context.AddObject(
+                    this.tableName,
+                    new EventEntity
+                        {
+                            PartitionKey = partitionKey,
+                            RowKey = eventData.Version.ToString("D10"),
+                            EventType = eventData.EventType,
+                            Payload = eventData.Payload
+                        });
+            }
+
+            // TODO: update record saying that there is a pending event to publish to the service bus
+
+            // TODO: error handling and retrying
+            try
+            {
+                context.SaveChanges(SaveChangesOptions.Batch);
+
+            }
+            catch (DataServiceRequestException ex)
+            {
+                var inner = ex.InnerException as DataServiceClientException;
+                if (inner != null && inner.StatusCode == 409)
+                {
+                    throw new ConcurrencyException();
+                }
+
+                throw;
+            }
+        }
+    }
+
+    public class EventData
+    {
+        public int Version { get; set; }
+        public string EventType { get; set; }
+        public string Payload { get; set; }
+    }
+
+    public class EventEntity : TableServiceEntity
+    {
+        public string EventType { get; set; }
+
+        public string Payload { get; set; }
+    }
+}
