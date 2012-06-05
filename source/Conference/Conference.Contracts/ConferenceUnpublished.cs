cf80321aaaf0366572535cf65337b2aff5e53f9c
Daniel Cazzulino (kzu) <daniel@cazzulino.com>
2012-06-05 09:20:42 -0300

    Added documentation comments to conference management contracts.

--- source/Conference/Conference.Contracts/ConferenceUnpublished.cs
+++ source/Conference/Conference.Contracts/ConferenceUnpublished.cs
@@ -16,6 +16,10 @@ namespace Conference
     using System;
     using Infrastructure.Messaging;
 
+    /// <summary>
+    /// Event published whenever a previously public conference 
+    /// is made private by unpublishing it.
+    /// </summary>
     public class ConferenceUnpublished : IEvent
     {
         public Guid SourceId { get; set; }
