a13ea479559291491d2108d9c5329d86ad29b9b0
Julian Dominguez <julian_domin@hotmail.com>
2012-06-07 16:52:33 -0700

    #509 Publish events in parallel

--- source/Infrastructure/Azure/Infrastructure.Azure.Tests/EventSourcing/EventStoreBusPublisherFixture.cs
+++ source/Infrastructure/Azure/Infrastructure.Azure.Tests/EventSourcing/EventStoreBusPublisherFixture.cs
@@ -14,6 +14,7 @@
 namespace Infrastructure.Azure.Tests.EventSourcing.EventStoreBusPublisherFixture
 {
     using System;
+    using System.Diagnostics;
     using System.Linq;
     using System.Threading;
     using Infrastructure.Azure.EventSourcing;
@@ -54,7 +55,7 @@ namespace Infrastructure.Azure.Tests.EventSourcing.EventStoreBusPublisherFixture
 
             sut.SendAsync(partitionKey);
 
-            Assert.True(sender.ResetEvent.WaitOne(3000));
+            Assert.True(sender.SendSignal.WaitOne(3000));
             cancellationTokenSource.Cancel();
         }
 
@@ -119,7 +120,7 @@ namespace Infrastructure.Azure.Tests.EventSourcing.EventStoreBusPublisherFixture
 
             for (int i = 0; i < pendingKeys.Length; i++)
             {
-                Assert.True(sender.ResetEvent.WaitOne(5000));
+                Assert.True(sender.SendSignal.WaitOne(5000));
             }
             cancellationTokenSource.Cancel();
         }
@@ -130,7 +131,7 @@ namespace Infrastructure.Azure.Tests.EventSourcing.EventStoreBusPublisherFixture
             for (int i = 0; i < pendingKeys.Length; i++)
             {
                 string expectedMessageId = string.Format("{0}_{1}", pendingKeys[i], version);
-                Assert.Equal(expectedMessageId, sender.Sent.ElementAt(i).MessageId);
+                Assert.True(sender.Sent.Any(x => x.MessageId == expectedMessageId));
             }
         }
 
@@ -154,6 +155,111 @@ namespace Infrastructure.Azure.Tests.EventSourcing.EventStoreBusPublisherFixture
             {
                 queue.Verify(q => q.DeletePending(pendingKeys[i], rowKey));
             }
+        }
+    }
+
+    public class given_event_store_with_events_after_it_is_started : IDisposable
+    {
+        private Mock<IPendingEventsQueue> queue;
+        private MessageSenderMock sender;
+        private string version;
+        private string[] partitionKeys;
+        private string rowKey;
+        private EventStoreBusPublisher sut;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public given_event_store_with_events_after_it_is_started()
+        {
+            this.version = "0001";
+            this.rowKey = "Unpublished_" + version;
+
+            this.partitionKeys = Enumerable.Range(0, 100).Select(i => "Key" + i).ToArray();
+            this.queue = new Mock<IPendingEventsQueue>();
+            queue.Setup(x => x.GetPending(It.IsAny<string>())).Returns<string>(
+                key => new[]
+                           {
+                               Mock.Of<IEventRecord>(
+                                   x => x.PartitionKey == key
+                                        && x.RowKey == rowKey
+                                        && x.TypeName == "TestEventType"
+                                        && x.SourceId == "TestId"
+                                        && x.SourceType == "TestSourceType"
+                                        && x.Payload == "serialized event")
+                           });
+            queue.Setup(x => x.GetPartitionsWithPendingEvents()).Returns(Enumerable.Empty<string>());
+            this.sender = new MessageSenderMock();
+            this.sut = new EventStoreBusPublisher(sender, queue.Object);
+            this.cancellationTokenSource = new CancellationTokenSource();
+            sut.Start(cancellationTokenSource.Token);
+        }
+
+        [Fact]
+        public void when_sending_multiple_partitions_immediately_then_publishes_all_events()
+        {
+            this.sender.SendWaitHandle = null;
+            for (int i = 0; i < partitionKeys.Length; i++)
+            {
+                this.sut.SendAsync(partitionKeys[i]);
+            }
+
+            var stopwatch = new Stopwatch();
+            var timeout = TimeSpan.FromSeconds(20);
+            stopwatch.Start();
+            while (sender.Sent.Count < partitionKeys.Length && stopwatch.Elapsed < timeout)
+            {
+                Thread.Sleep(300);
+            }
+
+            Assert.Equal(partitionKeys.Length, sender.Sent.Count);
+            for (int i = 0; i < partitionKeys.Length; i++)
+            {
+                string expectedMessageId = string.Format("{0}_{1}", partitionKeys[i], version);
+                Assert.NotNull(sender.Sent.Single(x => x.MessageId == expectedMessageId));
+            }
+        }
+
+        [Fact]
+        public void when_send_takes_time_then_still_publishes_events_concurrently_with_max_parallelism_degree_of_10()
+        {
+            var degreeOfParallelism = 10;
+
+            var resetEvent = new ManualResetEvent(false);
+            this.sender.SendWaitHandle = resetEvent;
+            for (int i = 0; i < partitionKeys.Length; i++)
+            {
+                this.sut.SendAsync(partitionKeys[i]);
+            }
+
+            var stopwatch = new Stopwatch();
+            var timeout = TimeSpan.FromSeconds(20);
+            stopwatch.Start();
+            while (sender.Sent.Count < degreeOfParallelism && stopwatch.Elapsed < timeout)
+            {
+                Thread.Sleep(500);
+            }
+
+            Assert.Equal(degreeOfParallelism, sender.Sent.Count);
+
+            resetEvent.Set();
+
+            // once all events can be sent, verify that it sends all.
+            stopwatch.Restart();
+            while (sender.Sent.Count < partitionKeys.Length && stopwatch.Elapsed < timeout)
+            {
+                Thread.Sleep(300);
+            }
+
+            Assert.Equal(partitionKeys.Length, sender.Sent.Count);
+            for (int i = 0; i < partitionKeys.Length; i++)
+            {
+                string expectedMessageId = string.Format("{0}_{1}", partitionKeys[i], version);
+                Assert.NotNull(sender.Sent.Single(x => x.MessageId == expectedMessageId));
+            }
+        }
+
+        public void Dispose()
+        {
+            this.cancellationTokenSource.Cancel();
         }
     }
 }
